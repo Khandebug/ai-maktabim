2062061c438eb2c9e121e30aedd7722527ee3c46
Khandebug <hrhrjtjobroiwh@gmail.com>
2026-07-01 11:25:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -272,4 +272,4 @@ def mentor_chat(req: MessageRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
